76ae7e7a581eff800a1e6b1dcc9cb905247be1a6
Tarun_Silam <tarun79767@gmail.com>
2024-11-03 12:20:21 +0530

    added google signin and sign out functions

--- firebase2/src/App.jsx
+++ firebase2/src/App.jsx
@@ -1,24 +1,44 @@
 import './App.css'
 import app from './firebase'
 import SignUpPage from './pages/SignUp';
-import {getAuth,createUserWithEmailAndPassword} from 'firebase/auth'
+import {getAuth,onAuthStateChanged,signOut} from 'firebase/auth'
 import SignInPage from './pages/SignIn';
+import { useEffect,useState } from 'react';
+
+
 const auth=getAuth(app);
 
 function App() {
 
-  const signUpUser=()=>{
-    createUserWithEmailAndPassword(auth,
-      'tarun79767@gmail.com',
-      'Tarun77@@'
-    ).then((value)=>console.log(value));
+  const [user,setUser]=useState(null);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user){
+      setUser(user);
+      console.log(user)
+      }
+      else{
+        console.log('You are logged out')
+        setUser(null);
+      }
+    })
+  },[])
+
+  if(user==null){
+    return(
+      <>
+      <h1>FireBase Auth</h1>
+      <SignUpPage/>
+      <SignInPage/>
+      </>
+    )
   }
 
   return (
     <>
-    <h1>FireBase Auth</h1>
-    <SignUpPage/>
-    <SignInPage/>
+    <h1>Hello {(user.displayName)?user.displayName:user.email}</h1>
+    <button onClick={()=>signOut(auth)}>Logout</button>
     </>
   )
 }
